84c428abde247b98b250531935ae1eac2da94f83
Tanya117 <tanyakhosla1@gmail.com>
2021-09-15 14:24:34 -0500

    set up gethub

---
+++ IntelliJFiles/src/unit_1main_method/MainMethod.java
@@ -0,0 +1,15 @@
+package unit_1main_method;
+
+public class MainMethod {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        int a = 82, b = 47;
+        double c= 47.0;
+
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a/c);
+        System.out.println(a%c);
+    }
+}
