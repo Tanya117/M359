d1cdbbdb5de02a6abbbe8c543e2e19427ec4ced8
Tanya117 <tanyakhosla1@gmail.com>
2021-10-21 08:09:52 -0500

    lab

--- IntelliJFiles/src/unit_4/PigLatin.java
+++ IntelliJFiles/src/unit_4/PigLatin.java
@@ -2,53 +2,49 @@ package unit_4;
 
 public class PigLatin {
 
-        public static String translateWordToPigLatin (String w) {
-           //while (w.indexOf(" not space"){ //while loop runs when there are no spaces
-            if (w.substring(0, 1).equalsIgnoreCase("a")
-                    || w.substring(0, 1).equalsIgnoreCase("e")
-                    || w.substring(0, 1).equalsIgnoreCase("i")
-                    || w.substring(0, 1).equalsIgnoreCase("o")
-                    || w.substring(0, 1).equalsIgnoreCase("u")) {
+        public static String translateWordToPigLatin(String w) {
+            w = w.toLowerCase();
+            if (vowelChecker(w) == true) {
 
                 w += "yay";
-                return w;
-
-
-            } else {
-                String consonant = "";
-
-                if (w.indexOf("a") > 0) {
-                    consonant = w.substring(0, w.indexOf("a")); //black
-                } else if (w.indexOf("e") > 0) {
-                    consonant = w.substring(0, w.indexOf("e"));
-                } else if (w.indexOf("i") > 0) {
-                    consonant = w.substring(0, w.indexOf("i"));
-                } else if (w.indexOf("o") > 0) {
-                    consonant = w.substring(0, w.indexOf("o"));
-                } else if (w.indexOf("u") > 0) {
-                    consonant = w.substring(0, w.indexOf("u"));
-                } else if (w.indexOf("y") > 0) {
-                    consonant = w.substring(0, w.indexOf("y"));
-                }
-
-                w = w.substring(consonant.length()) + consonant + "ay";
-                return w;
-
             }
-       // }
-
+            else{
+                String consonant = "";
+                while(vowelChecker(w.substring(0,1)) != true) {
+                    consonant += w.substring(0,1);
+                    w = w.substring(1);
+                }
+                w = w + consonant + "ay";
+            }
+            return w;
         }
-//caps helper method called ^^
 
+        public static boolean vowelChecker(String w) {
+            if (w.substring(0, 1).equals("a") || w.substring(0, 1).equals("e") || w.substring(0, 1).equals("i")
+                    || w.substring(0, 1).equals("o") || w.substring(0, 1).equals("u")) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
 
         public static String toPigLatin(String phrase) {
             String pigPhrase = " ";
-            String str = phrase.substring(0,phrase.indexOf(" "));
-            while(phrase.indexOf(" ") > 0 ){ pigPhrase += translateWordToPigLatin(str);
-            phrase = phrase.substring(str.length());
-               }
-            return pigPhrase;
+
+            while(phrase.indexOf(" ") >= 0){
+                String specificWord = phrase.substring(0,phrase.indexOf(" "));
+                pigPhrase += translateWordToPigLatin(specificWord) + " ";
+                phrase = phrase.substring(phrase.indexOf(" ")+1);
+            }
+            pigPhrase += translateWordToPigLatin(phrase);
+            String letterOne = pigPhrase.substring(0,1).toUpperCase();
+            pigPhrase = pigPhrase.substring(1);
+            return letterOne + pigPhrase;
         }
+
+
+
 
     }
 
