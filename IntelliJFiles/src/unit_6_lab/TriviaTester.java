03bd0046b6b48f0599228e4d6c509954cd2c37af
Tanya117 <tanyakhosla1@gmail.com>
2021-12-03 08:20:18 -0600

    changes

--- IntelliJFiles/src/unit_6_lab/TriviaTester.java
+++ IntelliJFiles/src/unit_6_lab/TriviaTester.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 
 public class TriviaTester {
     public static void main(String[] args) throws FileNotFoundException {
+        File questions = new File("Trivia.txt");
+        Scanner inF = new Scanner(questions);
+        inF.nextLine();
+
+        int num = inF.nextLine();
+        inF.nextLine();
 
     }
 }
