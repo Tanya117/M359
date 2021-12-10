eace690902205252aa786915f145665189e6af25
Tanya117 <tanyakhosla1@gmail.com>
2021-12-10 08:19:19 -0600

    changes in TriviaGame

--- IntelliJFiles/src/unit_6_lab/TriviaGame.java
+++ IntelliJFiles/src/unit_6_lab/TriviaGame.java
@@ -1,4 +1,39 @@
 package unit_6_lab;
 
 public class TriviaGame {
+    private String question;
+    private int streak;
+    private int totalPoints;
+
+    public TriviaGame(int streak, int totalPoints){
+        this.question= question;
+        this.streak=streak;
+        this.totalPoints=totalPoints;
+    }
+
+    //getters and setters
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getStreak() {
+        return streak;
+    }
+
+    public void setStreak(int streak) {
+        this.streak = streak;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
 }
+
